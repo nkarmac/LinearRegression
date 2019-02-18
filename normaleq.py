49d52af983c8055671bdb930c59ec7a430f82110
Nick K <nick.karamanian@gmail.com>
2019-02-18 13:47:52 -0800

    added loss func

--- normaleq.py
+++ normaleq.py
@@ -12,6 +12,7 @@ def main():
     w_vec = 0   # weight vector
     y_vec = 0   # label vector
     x_vec = 0   # feature vector
+    loss_vec = 0   
 
     myfile = input("Please specify a data file: (eg \"data_10k_100.tsv\")\n")
     datafile = open(myfile)
@@ -26,6 +27,7 @@ def main():
             y_vec = np.empty((n,1))
             x_vec = np.empty((n,d))
             bias = np.ones((n,1))
+            loss_vec = np.empty((n,1))
             i += 1
             continue
         if i==2:    # skipping label lines
@@ -45,12 +47,23 @@ def main():
     # weight vector using normal equation
     w_vec = np.linalg.inv((np.transpose(x_vec) @ x_vec)) @ (np.transpose(x_vec) @ y_vec)
 
+
+    ### Printing Block
     for x in range(d+1):
         print("w%i" % (d-x), end='\t')
     print()
     
     for x in range(d+1):
         print("%f" % w_vec[x], end='\t' )
+    ###
+    
+    # Build loss function
+    for x in range(n):
+        loss_vec[x] = np.square(y_vec[x] - np.transpose(w_vec) @ x_vec[x])
+    
+    # Sum then average loss
+    TotalLoss = np.sum(loss_vec) / (2*n)
+    print("\nTotal Loss is: %f" % TotalLoss)
 
 
 if __name__ == "__main__":

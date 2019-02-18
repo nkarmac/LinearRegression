507c36553c66b5de77c9064150d5d1426a638d4c
Nick K <nick.karamanian@gmail.com>
2019-02-18 13:46:03 -0800

    added normal equation solution, prints weight vec

---
+++ normaleq.py
@@ -0,0 +1,52 @@
+# solves normal equation to learn linear regression models
+
+import sys
+import numpy as np 
+
+
+def main():
+
+    i = 0       # line num for iterating
+    n = 0       # num data pts
+    d = 0       # num features
+    w_vec = 0   # weight vector
+    y_vec = 0   # label vector
+    x_vec = 0   # feature vector
+
+    myfile = input("Please specify a data file: (eg \"data_10k_100.tsv\")\n")
+    datafile = open(myfile)
+    for line in datafile:
+        
+        if i==0:    # number of data pts
+            n = int(line)
+            i += 1
+            continue
+        if i==1:    # number of features
+            d = int(line)
+            y_vec = np.empty((n,1))
+            x_vec = np.empty((n,d))
+            bias = np.ones((n,1))
+            i += 1
+            continue
+        if i==2:    # skipping label lines
+            i += 1
+            continue
+        
+        mylist = np.fromstring(line, dtype=np.float, sep='\t')
+
+        y_vec[i-3] = [mylist[0]]
+        x_vec[i-3] = mylist[1:]
+        i += 1
+    
+    x_vec = np.append(x_vec,bias, axis=1)
+
+    w_vec = np.linalg.inv((np.transpose(x_vec) @ x_vec)) @ (np.transpose(x_vec) @ y_vec)
+
+    print(w_vec)
+
+
+
+
+
+if __name__ == "__main__":
+    main()

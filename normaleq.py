5f7f3c12665f2c7658fd35c4f9e492cb408fc35c
Nick K <nick.karamanian@gmail.com>
2019-02-18 14:08:39 -0800

    fixed print index

--- normaleq.py
+++ normaleq.py
@@ -48,8 +48,8 @@ def main():
 
 
     # ### Printing Block, uncomment for w output
-    # for x in range(1, d+1):
-    #     print("w%i" % (i), end='\t')
+    # for x in range(1, d+2):
+    #     print("w%i" % (x), end='\t')
     # print()
     
     # for x in range(d+1):
